5617e7d2931e20a70412e419f6880ba9b76199fb
ramzi <ramzikvrafikv@gmail.com>
2021-06-08 10:52:10 +0530

    axios module added

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./Banner.css"
 
 function Banner() {
+    useEffect(() => {
+        
+      
+    }, [])
     return (
         <div className="banner">
             <div className="content">
