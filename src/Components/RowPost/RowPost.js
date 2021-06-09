0a7c86695020eaf1e8674064401194ea8b389a27
ramzi <ramzikvrafikv@gmail.com>
2021-06-09 11:29:47 +0530

    size of row decreased using css dynamically by adding turnary operator

--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -18,7 +18,7 @@ function RowPost(props) {
             <h2>{props.title}</h2>
             <div className="posters">
                 {movies.map((obj)=>
-                                    <img className="poster" src={`${imageUrl+obj.backdrop_path}`} alt="poster" />
+                                    <img className={props.isSmall ? 'smallPoster' : 'poster'} src={`${imageUrl+obj.backdrop_path}`} alt="poster" />
                 )}
                  
                   
