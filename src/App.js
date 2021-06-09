56f3d97c1c712a51d98753c75276aaf47f7790f8
ramzi <ramzikvrafikv@gmail.com>
2021-06-09 12:05:52 +0530

    more rows added horror,comedy ,documentary, romance

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import NavBar from './Components/NavBar/NavBar'
 import "./App.css"
-
+import {originals,action,comedy,horror,documentaries,romance} from "./urls"
 import Banner from './Components/Banner/Banner'
 import RowPost from './Components/RowPost/RowPost'
 
@@ -11,8 +11,14 @@ function App() {
     <div>
       <NavBar/>
       <Banner/>
-      <RowPost  title="Netflix Originals" />
-      <RowPost  title="Action" isSmall  />
+      <RowPost url={originals} title="Netflix Originals" />
+      <RowPost url={action} title="Action" isSmall  />
+      <RowPost url={comedy} title="Comedy" isSmall  />
+      <RowPost url={horror} title="Horror" isSmall  />
+      <RowPost url={romance} title="Romance" isSmall  />
+      <RowPost url={documentaries} title="Documentaries" isSmall  />
+     
+     
     </div>
   )
 }
--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -1,11 +1,11 @@
 import React, { useEffect,useState} from 'react'
 import "./RowPost.css"
 import axios from "../../axios"
-import {API_KEY,imageUrl} from "../../Constants/Constants"
+import {imageUrl} from "../../Constants/Constants"
 function RowPost(props) {
     const [movies, setMovies] = useState([])
     useEffect(() => {
-        axios.get(`discover/tv?api_key=${API_KEY}&with_networks=213`).then((response)=>{
+        axios.get(props.url).then((response)=>{
             console.log(response.data);
             setMovies(response.data.results)
         }).catch(err=>{
@@ -18,7 +18,7 @@ function RowPost(props) {
             <h2>{props.title}</h2>
             <div className="posters">
                 {movies.map((obj)=>
-                                    <img className={props.isSmall ? 'smallPoster' : 'poster'} src={`${imageUrl+obj.backdrop_path}`} alt="poster" />
+                <img className={props.isSmall ? 'smallPoster' : 'poster'} src={`${imageUrl+obj.backdrop_path}`} alt="poster" />
                 )}
                  
                   
